4af40fbe4fe4896636719d5474c6d6a47864f6df
AgentNAM <necolai2013@gmail.com>
2024-10-03 19:28:14 -0700

    Added obstacles and Shooter component

---
+++ Assets/Scripts/TestScene/Sandbox.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sandbox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a; //declared variable a
+
+        a = 42; // assigning value 42 to variable a
+
+        int b = 43; // declaring and assigning a value to variable b
+
+
+
+        char c = '4';
+
+        double d = char.GetNumericValue(c);
+
+        Debug.Log(d);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
